fa91bd38b47ba1ff1da92124f44f84cadf4abc9d
BeastLe9enD <beastle9end@outlook.de>
2023-03-29 11:31:54 +0200

    ✨ Update

--- src/Preview.js
+++ src/Preview.js
@@ -5,8 +5,7 @@ function Preview() {
         <section id="preview" className="preview text-center sticky-top">
             <img src={PreviewImg} className="w-100"></img>
             <div>
-                <h1 class="animate__animated animate__zoomIn mb-5">Hi, I'm Lorenz!<br></br>I'm a software
-                    engineer<br></br> from Zell, Germany!</h1>
+                <h1 class="animate__animated animate__zoomIn mb-5" style={{color: 'white'}}>Hi, I'm Lorenz!<br/>Building software is my passion!</h1>
                 <a href="#work" className="animate__animated animate__zoomIn mt-5 big-button">My projects</a>
             </div>
         </section>
